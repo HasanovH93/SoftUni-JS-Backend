2da7a869b5f706febd5fdf0f80654e1b3b25f139
Hasan Hasanov <hasanovh14@gmail.com>
2022-09-13 22:27:42 +0200

    Demo

--- DEMO/index.js
+++ DEMO/index.js
@@ -1,26 +1,19 @@
 const http = require("http");
+const router = require('./router')
 const { catalogPage } = require("./controllers/catalogController");
-const { homePage, aboutPage } = require("./controllers/homeController");
+const { homePage, aboutPage ,defaultPage} = require("./controllers/homeController");
 
-const routes = {
-  "/": homePage,
-  "/about": aboutPage,
-  "/catalog": catalogPage,
-};
+router.register('/',homePage)
+router.register('/about',aboutPage)
+router.register('/catalog',catalogPage)
+router.register('default',defaultPage)
+
 
 const server = http.createServer((req, res) => {
-  console.log("Request received!");
   console.log(">>>", req.method, req.url);
 
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  console.log(url);
+  router.match(req,res)
 
-  const handler = routes[url.pathname];
-  if (typeof handler == "function") {
-    handler(req, res);
-  } else {
-    defaultPage(req, res);
-  }
-});
+  });
 
 server.listen(3000);
