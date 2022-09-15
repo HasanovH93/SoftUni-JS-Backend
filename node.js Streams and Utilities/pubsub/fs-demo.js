b12e0fda01feafb6f585428d52e3fe0fb30fe350
Hasan Hasanov <hasanovh14@gmail.com>
2022-09-15 21:53:32 +0200

    node js Streams and Utilities Demo

--- node.js Streams and Utilities/pubsub/fs-demo.js
+++ node.js Streams and Utilities/pubsub/fs-demo.js
@@ -1,15 +1,19 @@
 const   fs  = require("fs");
  
-const result = fs.readdirSync(".");
-const output = []
-for (let item of result) {
-  if (fs.statSync(`./${item}`).isDirectory()) {
-   output.push(item, "is a Directory");
-  } else {
-    output.push(item, "Is  a File");
-  }
-}
+const stream = fs.createReadStream('./summary.txt', {
+    highWaterMark: 3
+})
 
-fs.writeFileSync('./summary.txt',output.join('\n'), "utf-8")
+const file = []
+
+stream.on('data',chunk => {
+    console.log(chunk.toString());
+    file.push(chunk)
+});
+
+stream.on('end', () =>{
+    console.log('File Compiled')
+    console.log(file.join(''))
+})
 
 
--- node.js Streams and Utilities/pubsub/index.js
+++ node.js Streams and Utilities/pubsub/index.js
@@ -1,22 +1,37 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.method == "GET") {
-    res.write("OK");
-    res.end();
+    if (req.url == "/index.html") {
+      fs.readFile(
+        "./node.js Streams and Utilities/pubsub/static/index.html",
+        (err,file) => {
+          res.writeHead(200, {
+            "Content-Type": "text/html",
+          });
+          res.write(file);
+          res.end();
+        }
+      );
+    } else {
+      res.writeHead("404");
+      res.write("404 Not found");
+      res.end();
+    }
   } else if (req.method == "POST") {
     const body = [];
     req.on("data", (chunk) => {
       body.push(chunk);
     });
     req.on("end", () => {
-        const result = JSON.parse(body.join(''))
+      const result = JSON.parse(body.join(""));
       console.log(result);
       result.count++;
       res.writeHead(200, {
-        'Content-Type': 'application/json'
-      })
-      res.write(JSON.stringify(result))
+        "Content-Type": "application/json",
+      });
+      res.write(JSON.stringify(result));
       res.end();
     });
   }
