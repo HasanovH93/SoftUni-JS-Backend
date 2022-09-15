358141c5e8542c896b7493368d4e0094b09f73d9
Hasan Hasanov <hasanovh14@gmail.com>
2022-09-15 21:08:25 +0200

    node js Streams and Utilities Demo

--- node.js Streams and Utilities/pubsub/fs-demo.js
+++ node.js Streams and Utilities/pubsub/fs-demo.js
@@ -1,8 +1,14 @@
 const { promises: fs } = require('fs')
 start()
+show()
 async function start(){
 
    const data = await fs.readFile('./node.js Streams and Utilities/pubsub/demo.txt')
    console.log(data.toString())
+}
+
+async function show(){
+    const result = await fs.readdir('./')
+    console.log(result)
 }
 
