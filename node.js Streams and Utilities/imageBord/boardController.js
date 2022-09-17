23a804b709b4c061124a5f4d76f43d0d2acd2931
Hasan Hasanov <hasanovh14@gmail.com>
2022-09-17 22:42:08 +0200

    Exercise

--- node.js Streams and Utilities/imageBord/boardController.js
+++ node.js Streams and Utilities/imageBord/boardController.js
@@ -1,4 +1,10 @@
+const fs = require('fs')
+
 function homePage(req,res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
+    fs.createReadStream('./static/index.html').pipe(res)
     res.write('Home Page')
     res.end()
        
