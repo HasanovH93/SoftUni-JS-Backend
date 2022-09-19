2f4d3913d58b8d9de00e5773d8b389778dacadf4
Hasan Hasanov <hasanovh14@gmail.com>
2022-09-19 21:28:17 +0200

    Express and Handlebars Demo

--- express-demo/controllers/catalogController.js
+++ express-demo/controllers/catalogController.js
@@ -2,4 +2,10 @@ const router = require('express').Router()
 
 router.get('/', (req,res) => {
     res.render('catalog')
+});
+
+router.get('/:productId',(req,res) => {
+    res.render('details')
 })
+
+module.exports = router
--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const hbr = require('express-handlebars')
 const homeController = require('./controllers/homeController')
+const catalogController = require('./controllers/catalogController')
 
 const handlebars = hbr.create({
     extname: '.hbs',
@@ -12,6 +13,7 @@ app.engine('.hbs',handlebars.engine);
 app.set('view engine', '.hbs');
 
 app.use(homeController);
+app.use('/catalog', catalogController)
 
 
 app.listen(3000)
