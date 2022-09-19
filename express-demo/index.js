b6784d900f4e501cacdcbc9c9897509b59d20f47
Hasan Hasanov <hasanovh14@gmail.com>
2022-09-19 21:06:14 +0200

    Express and Handlebars Demo

---
+++ express-demo/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const hbr = require('express-handlebars')
+
+const handlebars = hbr.create({
+    extname: '.hbs',
+});
+
+const app = express()
+
+app.engine('.hbs',handlebars.engine);
+app.set('view engine', '.hbs');
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+app.listen(3000)
