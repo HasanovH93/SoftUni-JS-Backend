21335124b51b482405dbc97e8d5ba5197385b1ff
Hasan Hasanov <hasanovh14@gmail.com>
2022-09-20 14:03:29 +0200

    Cat Shelter Express Demo

--- express-demo-catShelter/index.js
+++ express-demo-catShelter/index.js
@@ -1,1 +1,37 @@
-const express = require('express')
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.header({
+        'Content-Type': 'text/html'
+    })
+  res.write(`
+  <h1>Home</h1>
+  <ul>
+  <li><a href="/addBreed">Add Breed</li>
+  <li><a href="/addCat">Add Cat</li>
+  </ul>
+  `);
+  res.end();
+});
+
+app.get("/addBreed", (req, res) => {
+  res.header({
+    "Content-Type": "text/html",
+  });
+  res.write("<h1>Add Breed</h1>");
+  res.end();
+});
+
+app.get("/addCat", (req, res) => {
+  res.header({
+    "Content-Type": "text/html",
+  });
+  res.write("<h1>Add Cat</h1>");
+  res.end();
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000...");
+});
