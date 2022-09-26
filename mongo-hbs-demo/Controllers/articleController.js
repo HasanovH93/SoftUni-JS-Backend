150b55a37faea89e94ddb4f66ea6bee726d9a417
HasanovH93 <hasanovh14@gmail.com>
2022-09-26 23:15:24 +0200

    MongoDB  - handlebars demo

--- mongo-hbs-demo/Controllers/articleController.js
+++ mongo-hbs-demo/Controllers/articleController.js
@@ -1,8 +1,21 @@
-const articleController = require('express').Router();
+const articleController = require("express").Router();
 
-articleController.get('/', (req, res) => {
-   res.render('article')
+articleController.get("/", (req, res) => {
+  res.render("article", {
+    title: ' - Articles',
+    articles: [
+      {
+        author: "John",
+        title: "Eh",
+        content: "Static Article 1",
+      },
+      {
+        author: "Jack",
+        title: "Woow",
+        content: "Static Article 2",
+      },
+    ]
+  });
 });
-
 
 module.exports = articleController;
