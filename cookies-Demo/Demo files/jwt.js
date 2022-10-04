8b84705072d36a0a70fdbbe35ea144ea50221e85
HasanovH93 <hasanovh14@gmail.com>
2022-10-04 22:12:21 +0200

    jsonwebtoken demo

---
+++ cookies-Demo/Demo files/jwt.js
@@ -0,0 +1,39 @@
+const app = require("express")();
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+
+const secret = "super secret password";
+
+app.use(cookieParser());
+app.use((req, res, next) => {
+  const token = req.cookies.token;
+  if (token) {
+    try {
+      const data = jwt.verify(token, secret);
+      req.user = data;
+    } catch (err) {
+        res.redirect('/login')
+    }
+  }
+  next();
+});
+app.get("/", (req, res) => {
+    if(req.user){
+        res.send('Hello, ' + req.user.username);
+    }else {
+      res.send('Hello, guest')
+    }
+});
+
+app.get("/jwt", (req, res) => {
+  const payload = {
+    username: "Hasan",
+    roles: ["user", "admin"],
+  };
+
+  const token = jwt.sign(payload, secret);
+  res.cookie("token", token);
+  res.send("Token saved");
+});
+
+app.listen(3000);
