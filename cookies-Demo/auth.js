b6b5cac8b28bbe30cd9aff9138707905f4ae499d
HasanovH93 <hasanovh14@gmail.com>
2022-10-04 11:59:56 +0200

    authentication demo

--- cookies-Demo/auth.js
+++ cookies-Demo/auth.js
@@ -1,43 +1,52 @@
-const express = require('express');
-const session = require('express-session');
+const crypto = require('crypto')
+const express = require("express");
+const session = require("express-session");
 
 const app = express();
 
-app.use(express.urlencoded({ extended:true }))
+app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
-    cookie: {secure:false},
+app.use(
+  session({
+    cookie: { secure: false },
     resave: false,
     saveUninitialized: true,
-    secret: 'asdsadasdsa'
+    secret: "asdsadasdsa",
+  })
+);
 
-}));
-
-app.get('/', (req, res) => {
-    console.log('>>> User: ' + (req.session.user || 'guest'));
-    if(req.session.user){
-        res.send(`<p>Hello, ${req.session.user}</p>`);  
-   }else {
-    res.send('<p>Hello, guest</p> <p>Please login: <a href="/login">login here</a>');  
-   }
-})
-app.get('/login', (req, res) => {
-       res.send(`<form action="/login" method="post">
+app.get("/", (req, res) => {
+  console.log(">>> User: " + (req.session.user || "guest"));
+  if (req.session.user) {
+    res.send(`<p>Hello, ${req.session.user}</p>`);
+  } else {
+    res.send(
+      '<p>Hello, guest</p> <p>Please login: <a href="/login">login here</a>'
+    );
+  }
+});
+app.get("/login", (req, res) => {
+  res.send(`<form action="/login" method="post">
        <label>Username: <input type="text" name="username"></label>
       <label>Password: <input type="password" name="password"></label>
       <input type="submit" value="Log in">
-       </form>`)
-})
+       </form>`);
+});
+const users = {
+  peter: "123456",
+  john: "qwerty",
+};
+app.post("/login", (req, res) => {
+  console.log("Login attempt");
+  if (
+    users[req.body.username] != undefined &&
+    users[req.body.username] == req.body.password
+  ) {
+    req.session.user = req.body.username;
+    res.redirect("/");
+  } else {
+    res.status(401).send("Incorrect username or password");
+  }
+});
 
-app.post('/login', (req, res) => {
-    console.log('Login attempt')
-    if(req.body.username == 'peter' && req.body.password == '123456'){
-        req.session.user = 'Peter';
-        res.redirect('/');
-    }else {
-        res.status(401).send('Incorrect username or password')
-    }
-  
-})
-
-app.listen(3000)
+app.listen(3000);
