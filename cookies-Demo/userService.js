814a7ad29fd6eaf6d28066c0b9d6b1e0de081cbb
HasanovH93 <hasanovh14@gmail.com>
2022-10-04 20:41:17 +0200

    authentication - bcrypt

--- cookies-Demo/userService.js
+++ cookies-Demo/userService.js
@@ -1,33 +1,41 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 
+const users = [];
 
-const users = []
+async function register(username, password) {
+  if (users.find((u) => u.username.toLowerCase() == username.toLowerCase())) {
+    throw new Error("Username is taken");
+  }
 
-async function register(username,password) {
-       if(users.find(u => u.username.toLowerCase() == username.toLowerCase())){
-        throw new Error('Username is taken')
-       }
-
-       const user = {
-        username,
-        hashedPassword: await bcrypt.hash(password,10)
-       }
-       users.push(user)
+  const user = {
+    username,
+    hashedPassword: await bcrypt.hash(password, 10),
+    failedAttempts: 0,
+  };
+  users.push(user);
 }
 
+async function login(username, password) {
+  const user = users.find(
+    (u) => u.username.toLowerCase() == username.toLowerCase()
+  );
 
-async function login(username,password) {
-     
-    const user = users.find(u => u.username.toLowerCase() == username.toLowerCase());
-
-    if(!user){
-        return false;
+  if (!user) {
+    return false;
+  } else {
+    const success = await bcrypt.compare(password, user.hashedPassword);
+    if (success) {
+        user.failedAttempts = 0;
+      return true;
+    } else {
+      user.failedAttempts++;
+      return false;
     }
-    return bcrypt.compare(password,user.hashedPassword);
+  }
 }
 
 module.exports = {
-    users,
-    register,
-    login,
-}
+  users,
+  register,
+  login,
+};
