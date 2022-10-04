4fe9e1cd3f16a89f9cd2ba44d86cf527a9422696
HasanovH93 <hasanovh14@gmail.com>
2022-10-04 10:20:12 +0200

    authentication demo

--- cookies-Demo/index.js
+++ cookies-Demo/index.js
@@ -7,13 +7,13 @@ app.use(session({
     secret: 'my secret code',
     saveUninitialized: true,
     resave: false,
-    cookie: {secure: true}
+    cookie: {secure: false}
 }))
 
 app.get('/', (req,res) => {
   console.log(req.session);
-    req.session.message = 'hello'
-    res.send('Hello')
+    req.session.visited =  (req.session.visited || 0) + 1
+    res.send(`Hello. Visit Counter ${req.session.visited}`)
 })
 
 app.listen(3000)
