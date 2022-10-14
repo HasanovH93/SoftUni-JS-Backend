0cce5bd9c3d0264089443bdd80eab28fdb0e8543
HasanovH93 <hasanovh14@gmail.com>
2022-10-14 20:19:21 +0200

    fix typo in Accessory model

--- Cubicle-Worshop/src/models/Accessory.js
+++ Cubicle-Worshop/src/models/Accessory.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+const {Schema,model } = require('mongoose');
 
-const accessorySchema = new mongoose.Schema({
+const accessorySchema = new Schema({
     name: {type: String, required: true},
     imageUrl: {type: String, required: [true, 'Img url is required'] , validate: [/https?:\/\//i,'Img url is invalid']},
     descriotion: {type:String, required: true, maxLength: 50}
