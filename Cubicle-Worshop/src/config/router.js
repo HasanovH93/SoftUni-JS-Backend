614b5874ef12a26f6298116494cffaf5e8ac3685
HasanovH93 <hasanovh14@gmail.com>
2022-10-14 19:56:08 +0200

    add accessory controller
    add accessory controller

--- Cubicle-Worshop/src/config/router.js
+++ Cubicle-Worshop/src/config/router.js
@@ -1,11 +1,14 @@
-const router = require('express').Router()
+const express = require('express')
 const createController = require('../controllers/createController');
 const homeController = require('../controllers/homeController');
+const accessoryController = require('../controllers/accessoaryController')
+const router = express.Router()
 
 
 
 router.use(homeController);
-router.use(createController);
+router.use('/cube',createController);
+router.use('/accessory',accessoryController)
 router.use('*', (req, res) => {
     res.render('404')
 })
