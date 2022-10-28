abe2c273276253a664c23aabf17014ad5bc100b3
HasanovH93 <hasanovh14@gmail.com>
2022-10-28 21:38:47 +0200

    exam prep

---
+++ exam-prep4/controllers/authController.js
@@ -0,0 +1,66 @@
+const { register, login } = require("../services/userService");
+const { parseError } = require("../util/parser");
+
+const authController = require("express").Router();
+
+authController.get("/register", (req, res) => {
+  //TODO replace with actual view by assignment
+  res.render("register", {
+    title: "Register page",
+  });
+});
+
+authController.post("/register", async (req, res) => {
+  try {
+    if (req.body.username == "" || req.body.password == "") {
+      console.log("All fields are required!");
+    }
+    if (req.body.password != req.body.repass) {
+      throw new Error("passwords dont match");
+    }
+    const token = await register(req.body.username,req.body.email, req.body.password);
+
+    //TODO check assignment to see if register creates session
+    res.cookie("token", token);
+    res.redirect("/"); //TODO replace with redirect by assignment
+  } catch (error) {
+    const errors = parseError(error);
+    res.render("register", {
+      title: "Registration Page",
+      errors,
+      body: {
+        email: req.body.email,
+      },
+    });
+  }
+});
+authController.get("/login", (req, res) => {
+  res.render("login", {
+    title: "Login Page",
+  });
+});
+
+authController.post("/login", async (req, res) => {
+  try {
+   const token =  await login(req.body.email, req.body.password);
+   //TODO replace with redirect by assignment
+   res.cookie('token',token);
+   res.redirect('/')
+  } catch (error) {
+    const errors = parseError(error);
+    res.render("login", {
+      title: "Login Page",
+      errors,
+      body: {
+        email: req.body.email,
+      },
+    });
+  }
+});
+
+authController.get('/logout', (req, res) => {
+  res.clearCookie('token');
+  res.redirect('/')
+})
+
+module.exports = authController;
